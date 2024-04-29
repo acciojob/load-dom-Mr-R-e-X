e975035e44b3ed998c70ec472683299063e0bc55
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-29 20:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //your JS code here. If required.
 
 document.addEventListener("DOMContentLoaded", () => {
-  let body = document.querySelector("body");
-  let p = document.createElement("p");
-  p.innerText = `DOM load success`;
-  body.appendChild(p);
+let textNode = document.createTextNode('DOM load success');
+      document.body.appendChild(textNode);
 });
